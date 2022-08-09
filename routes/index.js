2a654170a0da4f8169fadb97f5dc5a731693792b
daviaquino87 <davi26031@gmail.com>
2022-08-08 22:29:56 -0300

    feat(route):create route new from create link

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Encurtador' });
 });
+
+router.post('/new', async(req,res,next) => {
+  const url = req.body.url;
+  const code = generateCode();
+
+  res.send(`${process.env.DOMAIN}${code}`);
+})
+
+function generateCode(){
+  let text = " ";
+  const possibility = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlmnopqrstuvwxyz0123456789";
+  for(i =0; i < 5; i++){
+    text += possibility.charAt(Math.floor(Math.random() * possibility.length));
+    return text;  
+  }
+
+}
 
 module.exports = router;
